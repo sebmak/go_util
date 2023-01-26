ba4b0df55aa060c9b6f513a855aad95c2a14f870
Sebastian Makinson <sebastian.makinson@gmail.com>
2023-01-26 14:43:28 -0700

    Added Loki as a log option

--- log.go
+++ log.go
@@ -2,13 +2,37 @@ package goutils
 
 import (
 	"github.com/sirupsen/logrus"
+	"github.com/yukitsune/lokirus"
 )
 
 var (
 	logger *logrus.Logger
 )
 
-//Logger returns a new logger
+func getLokiHook() *lokirus.LokiHook {
+
+	// Configure the Loki hook
+	opts := lokirus.NewLokiHookOptions().
+		// Grafana doesn't have a "panic" level, but it does have a "critical" level
+		// https://grafana.com/docs/grafana/latest/explore/logs-integration/
+		WithLevelMap(lokirus.LevelMap{logrus.PanicLevel: "critical"}).
+		WithFormatter(&logrus.JSONFormatter{}).
+		WithStaticLabels(lokirus.Labels{
+			"app":         GetEnvVariable("APP_NAME", "unset"),
+			"environment": GetEnvVariable("ENVIRONMENT", "development"),
+		}).
+		WithBasicAuth(GetEnvVariable("LOKI_USER", ""), GetEnvVariable("LOKI_PASS", "")) // Optional
+
+	return lokirus.NewLokiHookWithOpts(
+		GetEnvVariable("LOKI_URL", ""),
+		opts,
+		logrus.InfoLevel,
+		logrus.WarnLevel,
+		logrus.ErrorLevel,
+		logrus.FatalLevel)
+}
+
+// Logger returns a new logger
 func Logger() *logrus.Logger {
 	if logger == nil {
 
@@ -17,6 +41,10 @@ func Logger() *logrus.Logger {
 		logLevel, err := logrus.ParseLevel(GetEnvVariable("LOG_LEVEL", "info"))
 		if err == nil {
 			log.SetLevel(logLevel)
+		}
+
+		if GetEnvVariable("LOKI_URL", "") != "" {
+			log.AddHook(getLokiHook())
 		}
 
 		logger = log
